d77e4a3f28cb046b45f2b90866a6cff455493951
LukasGraz <lukasgraz99@gmail.com>
2025-02-14 00:24:34 +0100

    V0.2

--- docs/R/data_prep.R
+++ docs/R/data_prep.R
@@ -7,26 +7,27 @@ D_varlookup <- read_excel("data/df_varlookup_for_lukas.xlsx")
 D_types_orig <- sapply(D_orig, class)
 D_types_lookup <- setNames(D_varlookup$vartype, D_varlookup$variable)
 
-# check if all variables in D_orig are in D_varlookup & vice versa
+# Check if all variables in D_orig are in D_varlookup & vice versa
 # names(D_types_orig)[which(!(names(D_types_orig) %in% names(D_types_lookup)))]   # NO !!!
-# names(D_types_lookup)[which(!(names(D_types_lookup) %in% names(D_types_orig)))] # yes
+# names(D_types_lookup)[which(!(names(D_types_lookup) %in% names(D_types_orig)))] # Yes
 
-# # some variables are duplicated in D_varlookup
+# Some variables are duplicated in D_varlookup
 # D_types_lookup[duplicated(names(D_types_lookup))]
 
 
-# SUBSET OF DATA
+# SUBSET OF DATA ----
 # Define categories of variables to include
 # Mediators
 Mediator_vars <- c(
-  "FEELNAT", # values: [1-7]
-  "LNOISE", # LNOISE values: [1-5]
-  "LOC_SENS", # SENS = aggregated( LOC_... values: [1-5]) > using mean (?)
+  "FEELNAT",  # Values: [1-7]
+  "LNOISE",   # Values: [1-5]
+  "LOC_SENS", # SENS = aggregated(LOC_... values: [1-5]) > using mean (?)
   "LOC_SOUN", 
   "LOC_SCEN", 
   "LOC_VISE", 
   "LOC_VEGE", 
   "LOC_FAUN")
+
 PRS_orig_vars <- c(
   "LQUAL1",
   "LQUAL2",
@@ -48,17 +49,17 @@ GIS_vars <- c(
   sqrt="VIS5K",
   "RL_NDVI",
   "RL_NOISE",
-  # "HM_NDVI", # Include myself (i.e. not in varlookup), correct?
+  # "HM_NDVI",  # Include myself (i.e., not in varlookup) correct?
   # "HM_NOISE",
   sqrt="DISTKM",
-  sqrt="JNYTIME", # outlier
+  sqrt="JNYTIME",  # Outlier
   sqrt="STRIMP123",
   sqrt="STRIMP999"
 )
 
 Moderator_vars <- c("HM_NOISE")
 
-# dictionary:
+# Dictionaries for value mapping
 Dicts <- list(
   dict1 = c(
     "01_SDisagr" = 1,
@@ -91,7 +92,7 @@ Dicts <- list(
   dict5 = c(
     "01_NotDom" = 1,
     "02_RNotDom" = 2,
-    # no 3 in data
+    # No 3 in data
     "04_RDom" = 4,
     "05_VeryDom" = 5),
   dict6 = c(
@@ -130,7 +131,6 @@ Dicts <- list(
     "03_Neither-nor" = 3,
     "04_RGood" = 4,
     "05_VGood" = 5),
-#  [1] "00" "02" "01" "03" "07" NA   "04" "09" "08" "05" "10" "06"
   dict13 = c(
     "00" = 0,
     "01" = 1,
@@ -143,8 +143,6 @@ Dicts <- list(
     "08" = 8,
     "09" = 9,
     "10" = 10),
-# [1] "2-3xMonth"   "More1xWeek"  "Less1xMonth" "7xWeek"      NA           
-# [6] "1xWeek" 
   dict14 = c(
     "Less1xMonth" = 1,
     "2-3xMonth" = 2,
@@ -183,70 +181,71 @@ Dicts <- list(
 vars_w_few_lvls <- names(D)[sapply(D, function(x) length(unique(x)) < 9)]
 list_w_few_lvls <- lapply(D[vars_w_few_lvls], unique)
 
-# check if all values are in our dictionary
+# Check if all values are in our dictionary
 stopifnot(all(
   (D_orig[grep("LQUAL", names(D), value = TRUE)] |> as.matrix() |> table() |> names() == names(Dicts$dict1))
 ))
 
-# check all variabels in vars_w_few_lvls if the values are in some dictionary. If so, replace the values with the dictionary values
-for (varnam in vars_w_few_lvls){
+# Check and replace values using dictionaries
+for (varnam in vars_w_few_lvls) {
   found <- FALSE
-  for (dict in Dicts){
-    if (all(names(dict) %in% list_w_few_lvls[[varnam]])){
+  for (dict in Dicts) {
+    if (all(names(dict) %in% list_w_few_lvls[[varnam]])) {
       found <- TRUE
       D[[varnam]] <- unname(dict[D_orig[[varnam]]])
       break
     }
   }
-  if (interactive() && !found){
-    print(paste("Warning: no dictionary found for variable", varnam))
+  if (interactive() && !found) {
+    print(paste("Warning: No dictionary found for variable", varnam))
     print(list_w_few_lvls[[varnam]])
-    }
+  }
 }
 
+# Type checking and warnings
 D_types <- sapply(D, class)
-for (varnam in names(D_types_lookup)){
+for (varnam in names(D_types_lookup)) {
   vartype <- D_types_lookup[varnam]
   if (vartype == "GIS") 
     next
-  # if types not equal print warning
-  if (interactive() && D_types[varnam] != vartype){
-    print(paste("Warning: variable", varnam, "has type", D_types[varnam], "but should be", vartype, "  nlevels:", length(unique(D[[varnam]]))))
-    if (D_types[varnam] == "character"){
+  if (interactive() && D_types[varnam] != vartype) {
+    print(paste("Warning: Variable", varnam, "has type", D_types[varnam], "but should be", vartype, "  nlevels:", length(unique(D[[varnam]]))))
+    if (D_types[varnam] == "character") {
       print(unique(D[[varnam]]))
-    }}
+    }
+  }
 }
 
 
-# FILTERING --------------------
-# 1. FEELNATURE and NOISEANNOY_1 and HM/RL_NDVI and HM/RL _NOISE exclude
-#NULL!
-# 2. ACTIVITY exclude NA and 6 (no outdoor activity in the past 4 weeks)
+# FILTERING ----
+# 1. FEELNATURE and NOISEANNOY_1 and HM/RL_NDVI and HM/RL_NOISE exclude NULL
+# 2. ACTIVITY exclude NA and 6 (no outdoor activity in past 4 weeks)
 # 3. HEADPHNE exclude 2 and 3 (wearing headphones)
 # 4. MAPDATA exclude 0 (not mapped RL)
-# 5. DISTKM excludes values above of the 90th percentile (extremely large values) and
-# 6. DURATION excludes values above of the 90th percentile (extremely large values)
-# 7. JOURNEY TIME excludes values above of the 99th percentile (extremely large values)
+# 5. DISTKM excludes values above 90th percentile
+# 6. DURATION excludes values above 90th percentile
+# 7. JOURNEY TIME excludes values above 99th percentile
 # 8. HM_NOISE exclude NA
-# __Database Cleaning
+
+# Database Cleaning
 KeepDF <- with(D,
   data.frame(
       #  !is.na(ACTIVITY) &
       #  !grepl('I have never been outside', ACTIVITY) &
     Headphone = !is.na(HEADPHNE) & HEADPHNE == "No",
-    Distance = DISTKM <= quantile(DISTKM, 0.9, na.rm=TRUE),
-    Duration = REC_DUR <= quantile(REC_DUR, 0.95, na.rm=TRUE) | is.na(REC_DUR),
-    JourneyTime = JNYTIME <= quantile(JNYTIME, 0.99, na.rm=TRUE) | is.na(JNYTIME),
+    Distance = DISTKM <= quantile(DISTKM, 0.9, na.rm = TRUE),
+    Duration = REC_DUR <= quantile(REC_DUR, 0.95, na.rm = TRUE) | is.na(REC_DUR),
+    JourneyTime = JNYTIME <= quantile(JNYTIME, 0.99, na.rm = TRUE) | is.na(JNYTIME),
     HMNoise_NA = !is.na(HM_NOISE),
     Activity_NA = !is.na(D$REC_ACT),
-    PRS_all_NA = apply(D[PRS_orig_vars], 1, \(x)sum(is.na(x))<11)
+    PRS_all_NA = apply(D[PRS_orig_vars], 1, \(x) sum(is.na(x)) < 11)
   )
 )
 # cor(KeepDF) |> round(1) 
 ##-> Distance, Duration, JourneyTime are correlated (~.2) - no surprise
 ##-> PRS_ALL_NA and Headphone is strongly correlated (.6)
 
-cat("Nr of matches per filtercriteria (not disjoint)\n")
+cat("Number of matches per filter criteria (not disjoint)\n")
 colSums(!KeepDF) |> sort(decreasing = TRUE) |> print()
 
 KeepInd <- apply(KeepDF, 1, all)
@@ -254,11 +253,11 @@ cat("Keep ", sum(KeepInd), "of", nrow(D), "observations\n")
 D <- subset(D, KeepInd)
 # END FILTERING --------------------
 
-# TRANSFORMATIONS
-# distances(+ road lengths), time and proportions will be sqrt-transformed
-for (i in seq_along(GIS_vars)){
+# TRANSFORMATIONS ----
+# Distances (+ road lengths), time and proportions will be sqrt-transformed
+for (i in seq_along(GIS_vars)) {
   varnam <- GIS_vars[i]
-  if (names(GIS_vars)[i] == "sqrt"){
+  if (names(GIS_vars)[i] == "sqrt") {
     new_varnam <- paste0(varnam, "_sqrt")
     D[new_varnam] <- sqrt(D[varnam])
     GIS_vars[i] <- new_varnam
@@ -268,29 +267,28 @@ GIS_vars <- unname(GIS_vars)
 
 
 library(missForest)
-# PRS imputation (missForest)
-message("Impute PRS_orig_vars")
+# PRS imputation using missForest
+message("Imputing PRS_orig_vars")
 D[PRS_orig_vars] <- xfun::cache_rds({
-  missForest(as.matrix(D[PRS_orig_vars]))
+    missForest(as.matrix(D[PRS_orig_vars]))
   }, 
-  file = "PRS_imputation.rds", dir = "cache/",
+  file = "PRS_imputation.rds", 
+  dir = "cache/",
   hash = list(as.matrix(D[PRS_orig_vars]))
 )$ximp |> as.data.frame()
 
 
+# Principal Component Analysis
+pca. <- prcomp(scale(D[PRS_orig_vars], scale = FALSE))
+# See comments in index.qmd
 
-# PCA
-pca. <- prcomp(scale(D[PRS_orig_vars], scale=FALSE))
-# round(pca.$rotation, 2)
-#' SEE COMMENTS IN index.qmd
-
-X_centered <- D[PRS_orig_vars] |> scale(center=TRUE, scale=FALSE) |> as.matrix()
+X_centered <- D[PRS_orig_vars] |> scale(center = TRUE, scale = FALSE) |> as.matrix()
 X_reduced <- X_centered %*% pca.$rotation[,1:4]
 
-# cor(rowMeans(X_centered), X_centered %*% pca.$rotation[,1])
-#= 0.9996  -> 1st PC is really just a weighted average of all variables
+# First PC is really just a weighted average of all variables
+# cor(rowMeans(X_centered), X_centered %*% pca.$rotation[,1])  # = 0.9996
 
-# check if all responses have roughly the same mean and sd
+# Check if all responses have roughly the same mean and sd
 if(interactive())
   with(D, cbind(LQUAL1,LQUAL2,LQUAL3,LQUAL4,LQUAL5,LQUAL6,LQUAL7,LQUAL8,LQUAL9,LQUAL10,LQUAL11)) |> as.data.frame() |>
     sapply( \(x) c(mean=mean(x, na.rm=TRUE), sd=sd(x, na.rm=TRUE))) |> round(2)
@@ -302,33 +300,35 @@ D$EC <- rowMeans(D[, c("LQUAL7", "LQUAL8", "LQUAL9")], na.rm=TRUE)
 D$ES <- rowMeans(D[, c("LQUAL10","LQUAL11")], na.rm=TRUE)
 
 D[c("PC1", "PC2", "PC3", "PC4")] <- as.data.frame(X_centered %*% pca.$rotation[,1:4])
-PRS_vars <- c("LA","BA","EC","ES", "PC1", "PC2", "PC3", "PC4")
+PRS_vars <- c("LA", "BA", "EC", "ES", "PC1", "PC2", "PC3", "PC4")
 
-message("TODO: remove pca ?")
+message("TODO: Remove PCA?")
 
 
-# before prediction machines, use missForest to impute NAs
-message("impute mediators & GIS_vars for mlr")
+# Prepare data for machine learning
+message("Imputing mediators & GIS_vars for MLR")
 Dmlr <- D
-# mediator imputation (missForest)
+
+# Mediator imputation using missForest
 Dmlr[Mediator_vars] <- xfun::cache_rds({
-  missForest(as.matrix(Dmlr[Mediator_vars]))
+    missForest(as.matrix(Dmlr[Mediator_vars]))
   }, 
-  file = "Mediator_imputation.rds", dir = "cache/",
+  file = "Mediator_imputation.rds", 
+  dir = "cache/",
   hash = list(as.matrix(Dmlr[Mediator_vars]))
 )$ximp |> as.data.frame()
 
-# GIS imputation (missForest)
+# GIS imputation using missForest
 Dmlr[GIS_vars] <- xfun::cache_rds({
-  missForest(as.matrix(Dmlr[GIS_vars]))
+    missForest(as.matrix(Dmlr[GIS_vars]))
   }, 
-  file = "GIS_imputation.rds", dir = "cache/",
+  file = "GIS_imputation.rds", 
+  dir = "cache/",
   hash = list(as.matrix(Dmlr[GIS_vars]))
 )$ximp |> as.data.frame()
 
 
-
-# Train / Test
+# Split into training and test sets
 set.seed(123)
 train_ind <- sample(1:nrow(D), round(nrow(D) * 0.5))
 D_trn <- D[ train_ind,]
